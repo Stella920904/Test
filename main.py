56fd9cd2a0e71c2e8d004205a763bec03670e4f9
inat1358 <inat1358@gmail.com>
2023-06-23 15:22:04 +0900

    케이드 메인메소드

---
+++ main.py
@@ -0,0 +1,15 @@
+from TC_Login import TC_login_Class
+from TC_NF_Plan_FreeTrialStart import TC_NF_Plan_FreeTrialStart_Class
+
+
+if __name__ == "__main__":
+    
+    # 로그인 테스트 실행
+    # login = TC_login_Class()
+    # login.def_login()
+
+    # FreeTrial 테스트 실행
+    freetrial = TC_NF_Plan_FreeTrialStart_Class()
+    freetrial.FreetrialStart()
+
+    # 
